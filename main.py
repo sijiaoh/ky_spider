5c99d50147f52fe0e515d74f800a0c33c785e6a2
sijiaoh <sijiaoh@outlook.com>
2025-06-10 02:01:03 +0000

    Use headless chromium and grab multiple htmls

--- main.py
+++ main.py
@@ -3,24 +3,25 @@ from playwright.sync_api import sync_playwright
 
 URL = "https://emweb.securities.eastmoney.com/pc_hsf10/pages/index.html?type=web&code=SH605136&color=b#/cwfx"
 
-def grab_html(url):
+def grab_htmls(urls):
+    htmls = []
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
-        page.goto(url)
-        html = page.content()
+        for url in urls:
+            page.goto(url)
+            htmls.append(page.content())
         browser.close()
-    return html
+    return htmls
 
 def main():
-    html = grab_html(URL)
+    html = grab_htmls([URL])[0]
 
     soup = BeautifulSoup(html, "lxml")
 
     title = soup.select_one("title").text.strip()
     headings = [h.text.strip() for h in soup.select("h2")]
 
-    print(soup.prettify())
     print("TITLE:", title)
     print("H2s :", headings)
 
