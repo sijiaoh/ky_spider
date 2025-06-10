87f59036f4d62c4a0554dbf4f5ec7a2259cb6be4
sijiaoh <sijiaoh@outlook.com>
2025-06-10 14:35:36 +0000

    Add paging

--- main.py
+++ main.py
@@ -6,28 +6,62 @@ from io import StringIO
 URL = "https://emweb.securities.eastmoney.com/pc_hsf10/pages/index.html?type=web&code=SH605136&color=b#/cwfx"
 
 def grab_htmls(urls):
-    htmls = []
+    htmls = {}
+    for url in urls:
+        htmls[url] = []
+
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
         page = browser.new_page()
+
         for url in urls:
-            page.goto(url)
-            htmls.append(page.content())
+            while True:
+                page.goto(url)
+                htmls[url].append(page.content())
+
+                button = page.query_selector(".zyzb_table .next")
+                if button is None or not button.is_visible():
+                    break
+
+                table_html = page.query_selector(".zyzb_table").inner_html()
+                button.click()
+                page.wait_for_function(
+                    """
+                        (oldHtml) => {
+                            const newHtml = document.querySelector(".zyzb_table").innerHTML;
+                            return oldHtml !== newHtml;
+                        }
+                    """,
+                    arg=table_html,
+                    timeout=1000
+                )
+
         browser.close()
+
     return htmls
 
 def main():
-    html = grab_htmls([URL])[0]
+    htmls = grab_htmls([URL])
 
-    soup = BeautifulSoup(html, "lxml")
+    for url, pages in htmls.items():
+        tables = []
 
-    title = soup.select_one("title").text.strip()
-    print("TITLE:", title)
+        for i, page in enumerate(pages):
+            soup = BeautifulSoup(page, "lxml")
 
-    zyzb_table = soup.select_one(".zyzb_table .report_table .table1")
-    df = pd.read_html(StringIO(str(zyzb_table)))[0]
+            title = soup.select_one("title").text.strip()
+            print("TITLE:", title)
 
-    df.to_excel("build/zyzb_table.xlsx", index=False)
+            zyzb_table = soup.select_one(".zyzb_table .report_table .table1")
+            df = pd.read_html(StringIO(str(zyzb_table)))[0]
+
+            if i != 0:
+                df = df.iloc[:, 1:]
+
+            tables.append(df)
+
+        df = pd.concat(tables, axis=1, ignore_index=True)
+        df.to_excel("build/zyzb_table.xlsx", index=False)
 
 if __name__ == "__main__":
     main()
